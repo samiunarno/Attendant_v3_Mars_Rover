049b0b4d771f935a0af344b7e21dcac5f5f225d8
ESAM MAHMUD <121745434+esammahmud@users.noreply.github.com>
2023-12-22 14:31:19 +0600

    Add files via upload

---
+++ Website/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* Other content of your app */}
+    </div>
+  );
+}
+
+export default App;
